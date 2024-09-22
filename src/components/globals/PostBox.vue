9382c7360a29d0124a8da13d822f3bbd75a71e9f
Matthew Marchal <matthew.marchal@gmail.com>
2024-09-22 16:06:37 -0600

    added more profile content + styling

--- src/components/globals/PostBox.vue
+++ src/components/globals/PostBox.vue
@@ -66,7 +66,29 @@ async function getProfile() {
       <div id="post-list" class="container-fluid post-list px-5">
         <section v-if="activeProfile" class="row profile my-5">
           <div class="col-12">
-            <img :src="activeProfile.coverImg">
+            <img :src="activeProfile.coverImg" :alt="activeProfile.name" class="cover-img">
+          </div>
+          <div class="col-12 p-rel profile-head">
+            <div class="d-flex justify-content-end fs-2 gap-3">
+              <a :href="activeProfile.github" :title="activeProfile.github">
+                <i class="mdi mdi-github"></i>
+              </a>
+              <a :href="activeProfile.linkedin" :title="activeProfile.linkedin">
+                <i class="mdi mdi-linkedin"></i>
+              </a>
+              <a :href="activeProfile.resume" :title="activeProfile.resume">
+                <i class="mdi mdi-file-outline"></i>
+              </a>
+            </div>
+            <img :src="activeProfile.picture" :alt="activeProfile.name" class="profile-img">
+            <div v-if="activeProfile.graduated" class="graduate-icon fs-3">
+              <i class="mdi mdi-account-school"></i>
+            </div>
+          </div>
+          <div class="col-12">
+            <p>{{ activeProfile.class }}</p>
+            <h2>{{ activeProfile.name }}</h2>
+            <i>{{ activeProfile.bio }}</i>
           </div>
         </section>
         <PostForm />
@@ -91,6 +113,11 @@ async function getProfile() {
 
 
 <style lang="scss" scoped>
+a {
+  color: black;
+  text-decoration: none;
+}
+
 .post-box {
   max-height: calc(100vh - 64px);
 }
@@ -104,18 +131,48 @@ async function getProfile() {
   height: 64px;
 }
 
-.creatorImg {
-  width: 80px;
-  height: 80px;
+.cover-img {
+  width: 100%;
+  height: 15rem;
+  object-fit: cover;
+  object-position: center;
+}
+
+.profile-head {
+  min-height: 64px;
+}
+
+.graduate-icon {
+  position: absolute;
+  top: 20px;
+  left: 90px;
+  height: 40px;
+  aspect-ratio: 1/1;
+  border: 3px solid #cccccc;
+  border-radius: 50%;
+  background-color: white;
+  text-align: center;
+}
+
+.profile-img {
+  position: absolute;
+  top: -40px;
+  left: 20px;
+  width: 100px;
+  height: 100px;
   object-fit: cover;
   object-position: center;
   border-radius: 50%;
+  border: 3px solid #cccccc;
 }
 
 .profile {
-  position: relative;
   box-shadow: 1px 1px 10px #999999;
   background-color: var(--lightest);
   padding: 2rem;
+}
+
+.p-rel {
+  position: relative;
 }
 </style>
